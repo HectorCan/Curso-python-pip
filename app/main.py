dd69e628be00c3b11bd897b72824932f4d17046a
Hector Can <habacab@gmail.com>
2023-02-08 19:04:04 -0500

    added pandas for read and filter

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
 from read_csv import read_csv
 from utils import population_by_country, get_population
 from charts import generate_pie_chart, generate_bar_chart
-
+import pandas as pd
 
 def run():
+  '''  
   data = read_csv('./data.csv')
 
   # Se puede filtrar por continente
-  '''  
+
   labels = [c['Country/Territory'] for c in data]
   values = [c['World Population Percentage'] for c in data]
   
   generate_pie_chart(labels, values)
   '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'South America']
+
+  countries = df['Country/Territory']
+  percentages = df['World Population Percentage'].values
   
+  generate_pie_chart(countries, percentages)
+  
+  data = read_csv('./data.csv')
   country = input('Type Country => ')
 
   result = population_by_country(data, country)
